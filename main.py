ed9c0ec909f0e073ba69c0a2863eea4f5baf2eec
John Ortiz <jaortiz92@hotmail.com>
2023-04-28 08:36:43 -0500

    Add Dockerfile

--- main.py
+++ main.py
@@ -31,12 +31,16 @@ app.include_router(query)
 origins = [
     "http://localhost",
     "http://127.0.0.1:8080",
-    "http://192.168.0.3:8080"
+    "http://192.168.0.3:8080",
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
